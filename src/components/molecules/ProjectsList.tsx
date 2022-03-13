1b3e5d230703c1d66f59f1f1bde85e5fbfb4ecf5
Ghassen ELLOUMI <ghassen.elloumi@vo2-group.com>
2022-03-13 21:31:30 +0100

    Implementing infinite scroll

--- src/components/molecules/ProjectsList.tsx
+++ src/components/molecules/ProjectsList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {ProjectCard} from "./ProjectCard";
 import {makeStyles} from "@material-ui/core/styles";
 import {ProjectType} from "../../common/types/ProjectType";
@@ -17,9 +17,39 @@ const useProjectsListStyles = makeStyles({
 
 export const ProjectsList: React.FC<ProjectsListProps> = ({projects}) => {
     const classes = useProjectsListStyles();
+    const projectsPerPage = 20;
+
+    const [projectToShowOnPage, setProjectsToShowOnPage] = useState<Array<ProjectType>>([...projects.slice(0, projectsPerPage + 1)]);
+
+    let projectsNumber = projectsPerPage;
+
+
+    function handleScroll() {
+        const isAtBottom = document.documentElement.scrollHeight - document.documentElement.scrollTop <= document.documentElement.clientHeight;
+
+        if (isAtBottom) {
+
+            console.log('oooo')
+
+            // Load next posts
+            projectsNumber += projectsPerPage;
+
+            setProjectsToShowOnPage([...projects.slice(0, projectsNumber + 1)]);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, []);
+
+
     return (
         <div className={classes.container}>
-            {projects.map((project: ProjectType) => (
+            {projectToShowOnPage.map((project: ProjectType) => (
                 <ProjectCard
                     key={project.uniqueId}
                     date={project.creationDate}
